ad9d1572c9459ce4df06100ebb8d29b8d1d9f8d1
Adam on limbo-iv Manjaro <f3ydrus@posteo.net>
2021-01-11 13:41:30 +0000

    Working dateAdded and notetitle split for standalone notes

--- debug-bridge-scratch.js
+++ debug-bridge-scratch.js
@@ -5,8 +5,10 @@ var items = Zotero.getActiveZoteroPane().getSelectedItems();
 if (items) {
     // return items[0].getRelations()["dc:relation"]
     // return items[0].getNote();
-    return Zotero.ItemTypes.getName(items[0].itemTypeID) !== "note";
+    //return Zotero.ItemTypes.getName(items[0].itemTypeID) !== "note";
     // return items[0].getField("key");
     // return items[0].getField("title").split(/_(.+)/);
-    return JSON.stringify(items[0]) + "\nitem.isNote() : " + items[0].isNote();
+    //return JSON.stringify(items[0]) + "\nitem.isNote() : " + items[0].isNote();
+    const d = new Date(items[0].getField("dateAdded"));
+    return (d.getMonth() < 10 ? "0" : "") + (d.getMonth() + 1);
 }
